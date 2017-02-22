b585291381a83bbfa810a19d155edc607977937e
tingtingQi <tqi@uw.edu>
2017-02-22 02:41:29 -0800

    Update DataReaderplus.py

--- DataReaderplus.py
+++ DataReaderplus.py
@@ -191,48 +191,5 @@ user_num = DataReader().get_user_num()
 
 
 
-#############################################create score###############################
-
-contacts = pd.read_sql( "SELECT user_id, technology_id , count(*) as c_count FROM contacts group by user_id, technology_id", con = con)    
-clicks =  pd.read_sql( "SELECT user_id, clicked_technology_id as technology_id, count(*) as e_count FROM email_clicks group by user_id, technology_id", con = con)
-activities = pd.read_sql( "SELECT user_id, details FROM user_activities", con = con) 
-# extract technology_id from details
-technology_id = []
-for index, row in activities.iterrows():
-    start = row[1].find("Article_id") # finding start from "Article_id"
-    end = row[1].find("content")
-    tech_id = int(re.search(r'\d+', row[1][start:end]).group(0))
-    technology_id.append(tech_id)
-    
-#mapping tech_id with user_id   
-activities['technology_id'] = technology_id 
-#drop column detials 
-activities = activities.drop('details', 1)
-activities = activities.groupby(["user_id", "technology_id"]).size().reset_index(name = "v_count")
-
-#dfs outer joins
-
-score = pd.merge(contacts, clicks, how = 'outer').merge(activities, how = 'outer')
-
-#df split train/test
-np.random.seed(seed = 13579)
-n = len(score)
-#print n
-rand_order = np.arange(0,n)
-np.random.shuffle(rand_order)
-score['whether_train'] = np.zeros(len(score))
-train_index = list(rand_order[:int(n*.66)])
-score.loc[train_index, 'whether_train'] = 1
-
-#weight
-weight = np.array([[1, 1, 1]]).T
-#print weight
-
-#add total_score
-score["total_score"] = score[['c_count', "e_count", "v_count"]].fillna(0).dot(weight).sum(1) 
-
-#write df back to sql
-
-score.to_sql('score', con = con, if_exists = 'replace')
 
 
